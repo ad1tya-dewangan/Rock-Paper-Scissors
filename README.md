fd039581451658cbb18135ffebd236c2af0378f7
Aditya-Dewangan <adityadewangan845@gmail.com>
2026-01-11 12:30:11 +0530

    Added a polished UI to the Game with buttons and image changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Rock-Paper-Scissors
+# Rock-Paper-Scissors Game 
+
+A classic Rock-Paper-Scissors game with polished UI including buttons to make choices and play the game against computer.
+
+Score updates after every round, First to score 5 wins the game.
+
+It shows result after every round and declare winner whoever score 5 first.
+
+I used JavaScript EventListeners to make the buttons functional.
+
+Practiced Dom manipulation to change the images and results on the web page.
+
+Learned about various dom methods and events concept in javascript while building this project. 
--- script.js
+++ script.js
@@ -1,75 +1,150 @@
 let humanScore = 0;
 let computerScore = 0;
+const winScore = 5;
 
-//function for getting users's choice
-function getHumanChoice(){
-    let hc = prompt("Enter your choice(rock,paper or scissor): "); 
-    return hc.toLowerCase();
-}
+// a map to decide computer choice
+const compChoiceMap = {
+    1: "rock",
+    2: "paper",
+    3: "scissor"
+};
 
 //fuction for getting computer's choice
 function getComputerChoice(){
     let cc = Math.ceil(Math.random() * 3);
-    if(cc === 1){
-        return "rock";
-    }
-    if(cc === 2){
-        return "paper";
-    }
-    if(cc === 3){
-        return "scissor";
-    }
+    return compChoiceMap[cc];
 }
 
-//function to play a round 
+// a map to decide round winner
+const winAgainst = {
+    rock: "scissor",
+    paper: "rock",
+    scissor: "paper"
+};
+
+// function to play a round 
 function playRound(humanChoice, computerChoice){
 
-    console.log(`You chose: ${humanChoice}`);
-    console.log(`Computer chose: ${computerChoice}`);
-
     if(humanChoice === computerChoice){
-        console.log("That's a tie");
+        setTimeout(() => {
+            roundResult.textContent = "That's a tie";
+        }, 500);
+        return;
     }
 
-    if(humanChoice === "rock"){
-        if(computerChoice === "paper"){
-            console.log("You lose! Paper beats Rock");
-            computerScore++;
-        }
-        else if(computerChoice === "scissor"){
-            console.log("You win! Rock beats Scissor");
-            humanScore++;
-        }
+    if(winAgainst[humanChoice] === computerChoice){
+        setTimeout(() => {
+            roundResult.textContent = `You Win! ${humanChoice} beats ${computerChoice}`;
+        }, 500);
+        humanScore++;
     }
-
-    if(humanChoice === "paper"){
-        if(computerChoice === "scissor"){
-            console.log("You lose! Scissors beat Paper");
-            computerScore++;
-        }
-        else if(computerChoice === "rock"){
-            console.log("You win! Paper beats Rock");
-            humanScore++;
-        }
+    else{
+        setTimeout(() => {
+            roundResult.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
+        }, 500);
+        computerScore++;
     }
-
-    if(humanChoice === "scissor"){
-        if(computerChoice === "rock"){
-            console.log("You lose! Rock beat Scissor");
-            computerScore++;
-        }
-        else if(computerChoice === "paper"){
-            console.log("You win! Scissors beat Paper");
-            humanScore++;
-        }
-    }
-
 }
 
-const button = document.querySelectorAll(".btn");
+// image reference
+const humanImage = document.querySelector(".humanImage");
+const computerImage = document.querySelector(".computerImage");
 
-document.addEventListener("click", e => {
-    if(e.target.classList.contains(".btn")){
-        console.log(e.target.textContent);
+// a map to decide image choice
+const imageMap = {
+    rock: "/image/rock.png",
+    paper: "/image/paper.png",
+    scissor: "/image/scissor.png"
+};
+
+// function to change image on page
+function setHumanImage(choice){
+    humanImage.src = imageMap[choice];
+}
+
+function setComputerImage(choice, delay = 500){
+    setTimeout(() => {
+        computerImage.src = imageMap[choice];
+    }, delay);
+}
+
+// function changeImage(h,c){
+//     humanImage.src = imageMap[h];
+//     computerImage.src = imageMap[c];
+// }
+
+// score reference
+const hScore = document.querySelector(".hScore");
+const cScore = document.querySelector(".cScore");
+
+function showScore() {
+    setTimeout(() => {
+        hScore.textContent = `Score: ${humanScore}`;
+        cScore.textContent = `Score: ${computerScore}`;
+    }, 500);
+}
+
+// results reference
+const roundResult = document.querySelector(".round-result");
+const gameResult = document.querySelector(".game-result");
+
+// function to check who wins 5 round first
+function checkWinner(){
+    if(humanScore === winScore){
+        endGame("Congratulation! You win the Game 🎉");
     }
-})
+    else if(computerScore === winScore){
+        endGame("You lost! Better luck next time 😅");
+    }
+}
+
+function endGame(message){
+    gameResult.textContent = message;
+    disableButtons();
+}
+
+// buttons reference
+const btnGroup = document.querySelectorAll(".btn");
+
+// disabling buttons after winner is decided
+function disableButtons(){
+    btnGroup.forEach(btn => {
+        btn.disabled = true;
+    });
+}
+
+// enabling button when new-game is pressed
+function enableButtons(){
+    btnGroup.forEach(btn => {
+        btn.disabled = false;
+    });
+}
+
+// buttons click event handeler
+document.addEventListener("click", e => {
+    if(e.target.classList.contains("btn")){
+        let humanSelection = e.target.id;
+        let computerSelection = getComputerChoice();
+        // changeImage(humanSelection,computerSelection);
+        setHumanImage(humanSelection);
+        setComputerImage(computerSelection);
+        playRound(humanSelection, computerSelection);
+        showScore();
+        checkWinner();
+    }
+});
+
+const newGame = document.querySelector(".newGame-btn");
+
+// new-game button event handeler
+newGame.addEventListener("click", () => {
+    humanScore = 0;
+    computerScore = 0;
+    showScore();
+    humanImage.src = "/image/defaultHand-start.png";
+    computerImage.src = "/image/defaultHand-start.png";
+    roundResult.textContent = "";
+    gameResult.textContent = "";
+    enableButtons();
+});
+
